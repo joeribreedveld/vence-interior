2eec9e0f1dfbbd3bd6564eff2b5eacaf25370998
Duck1s <joeri.breedveld123@gmail.com>
2023-03-16 19:35:50 +0100

    feat(footer): setup footer html and component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,41 @@
+// Imports
+
+// Functions
+const Footer = () => (
+  <footer>
+    <section>
+      <h3>
+        VENCE Studio<span>.</span>
+      </h3>
+      <ul>
+        <li>
+          <p>About Studio</p>
+        </li>
+        <li>
+          <p>Archive</p>
+        </li>
+        <li>
+          <p>Contact Us</p>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a>Fb</a>
+        </li>
+        <li>
+          <a>Tw</a>
+        </li>
+        <li>
+          <a>Ln</a>
+        </li>
+      </ul>
+    </section>
+    <section>
+      <p>1088, North Street, Alexandria, AU</p>
+      <p>© 2022, All Rights Reserved</p>
+    </section>
+  </footer>
+);
+
+// Exports
+export default Footer;
